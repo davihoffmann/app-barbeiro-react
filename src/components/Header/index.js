20ffabd277440ac7a7e8fe53930780f5f18875de
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-06-12 10:58:30 -0300

    criado camponente do campo de upload da foto de perfil do usuário

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 import Notifications from '~/components/Notifications';
 
+import defaultiImageUser from '~/assets/no-user-image.png';
 import logo from '~/assets/logo-purple.svg';
 import { Container, Content, Profile } from './styles';
 
@@ -21,10 +22,7 @@ export default function Header() {
               <strong>Davi Hoffmann</strong>
               <Link to="/profile">Meu Perfil</Link>
             </div>
-            <img
-              src="https://api.adorable.io/avatars/50/abott@adorable.png"
-              alt="Davi Hoffmann"
-            />
+            <img src={defaultiImageUser} alt="Davi Hoffmann" />
           </Profile>
         </aside>
       </Content>
